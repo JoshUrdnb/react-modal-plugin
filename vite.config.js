05318539209247ffd61256652e93257009e75bf6
Josh Urdnb <joshua.urdanabia@gmail.com>
2025-06-02 12:41:34 +0200

    Configuration des fichiers vite.config et package.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: 'src/index.js',
+      name: 'ReactModalPlugin',
+      fileName: 'react-modal-plugin',
+    },
+    rollupOptions: {
+      // On s'assure d’externaliser React pour ne pas l’inclure dans le bundle
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 })
