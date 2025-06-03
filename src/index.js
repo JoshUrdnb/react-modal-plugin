009db7e1fd901299773e972c4eea67ff69f6280d
Josh Urdnb <joshua.urdanabia@gmail.com>
2025-06-03 15:53:29 +0200

    feat: modification de la clé d'export modalPlugin.css
    refactor : suppression des fichiers Hello World.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-// import HelloWorld from './lib/HelloWorld.jsx'
 import ModalPlugin from './lib/modalPlugin/ModalPlugin.jsx'
 
-// export { HelloWorld }
-// export default HelloWorld
 export default ModalPlugin
